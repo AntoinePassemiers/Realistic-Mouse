192f66fd224f3c536ac442da4ad7120130ac1a7d
AntoinePassemiers <apassemi@ulb.ac.be>
2018-09-20 13:04:31 +0200

    Bug fix: click distribution

--- rmm/distribution.py
+++ rmm/distribution.py
@@ -14,9 +14,24 @@ class Distribution:
         self.pdf = np.asarray(pdf)
         self.pdf = self.pdf / pdf.sum()
         self.proba = np.cumsum(self.pdf)
+        self.proba[0] = 0.
+        self.proba[-1] = 1.
     
-    def sample(self):
+    def sample(self, n_samples=1):
         threshold = random.random()
-        is_lower = np.diff((threshold < self.proba).astype(np.int))
-        sample = np.where(is_lower == 1)[0][0]
-        return sample
+        is_lower = np.diff((threshold <= self.proba).astype(np.int))
+        samples = np.where(is_lower == 1)[0]
+        return samples[0]
+
+
+if __name__ == '__main__':
+
+    pdf = np.load(CLICK_FILE_PATH)
+    d = Distribution(pdf)
+
+    samples = [d.sample() for i in range(1000)]
+
+    import matplotlib.pyplot as plt
+    plt.switch_backend('TKAgg')
+    plt.hist(samples)
+    plt.show()
